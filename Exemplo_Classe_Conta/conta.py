32a3d885ebfad829771fa72506c8355e85a6f757
jose-ewerton <jose-ewerton@escolar.ifrn.edu.br>
2021-03-01 03:34:57 -0300

    Testes __str__ e __repr__
    Testando funções!

--- Exemplo_Classe_Conta/conta.py
+++ Exemplo_Classe_Conta/conta.py
@@ -10,10 +10,16 @@ class Conta:
         self.saldo+=valor
         print(self.saldo)
 
-    
-    def __repr__(self):
+'''    
+    def __repr__(self):       #retorna uma string que descreve o ponteiro do objeto por padrão (se o programador não o definir).
             return '{},{}'.format(self.numero, self.titular)
 
-    def __str__(self):
+    def __str__(self): #retorna o objeto como string
         return self.__repr__()
-
+'''
+if ( __name__ =='__main__'):
+    print('Dentro da função!')
+    c = Conta(1,2,3,4)
+    print(c.__repr__ ())
+    print(c.__str__ ())
+    print(c)
