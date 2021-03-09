f89402380f217a021920a21cfee7f036ee9f4b64
jose-ewerton <jose-ewerton@escolar.ifrn.edu.br>
2021-03-09 20:15:26 -0300

    Att da aula 09/03/2021

--- aula11-12/TestarFuncionario.py
+++ aula11-12/TestarFuncionario.py
@@ -1,12 +1,11 @@
 from funcionario import Funcionario
 
-novoFuncionario = Funcionario()
+novoFuncionario = Funcionario()#crie uma instancia da classe = objeto
 novoFuncionario.nome = 'Luiz' #aqui eu estou criando um novo atributo e não acessando os da classe!
 
-novoFuncionario.cpf = '1234567890'
-print(novoFuncionario.nome,novoFuncionario.cpf)
+novoFuncionario.cpf = '1234567890' # método setter
+print(novoFuncionario.nome,novoFuncionario.cpf) # método getter
 
 print(novoFuncionario.__dict__) #lista no formato de dicionario o que tenho no objeto da classe
-
 
 
