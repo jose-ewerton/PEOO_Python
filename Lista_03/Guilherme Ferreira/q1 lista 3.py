488814c5e191135a23ec428f21243d7d0c4cd590
jose-ewerton <jose-ewerton@escolar.ifrn.edu.br>
2021-03-16 11:10:36 -0300

    Atualização do exercício de Guilherme Ferreira

--- Lista_03/Guilherme Ferreira/q1 lista 3.py
+++ Lista_03/Guilherme Ferreira/q1 lista 3.py
@@ -1,25 +1,8 @@
-<<<<<<< HEAD
-def numero(*N):
-    N = []
-    while 0<1:
-        N.append(float(input("digite um numero: ")))
-        print(sum(N))
-
-numero(1,2,2,3,4,5)
-=======
 def numero(*args):
     N = []
-    N += args
-    cont = 0
-    while cont < 1:
-        
-        N.append(float(input("digite um numero: ")))
-        continuar = str(input("continuar? S/N: "))
-        if continuar == "S":
-            cont = 0
-        else:
-            cont = 1
-            
-    print(sum(N))
-numero(0)
->>>>>>> 692e22ea2340ba349368fae9b74aaf070c93cf4a
+    N += args 
+    print(len(N),sum(N))
+
+numero(1,2,2,3,4,5)
+numero()
+numero(1,2,2,3)
