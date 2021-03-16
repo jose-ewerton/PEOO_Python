dbac5ea2efbca3ab989d590b24746d63ea0f5569
jose-ewerton <jose-ewerton@escolar.ifrn.edu.br>
2021-03-16 10:44:17 -0300

    Coreções de exercícios

--- Lista_03/Guilherme Ferreira/q1 lista 3.py
+++ Lista_03/Guilherme Ferreira/q1 lista 3.py
@@ -1,7 +1,7 @@
-def numero(N):
+def numero(*N):
     N = []
-    cont = 0
     while 0<1:
         N.append(float(input("digite um numero: ")))
         print(sum(N))
-numero(1)
+
+numero(1,2,2,3,4,5)
--- Lista_03/Ian Guilherme/Q1.py
+++ Lista_03/Ian Guilherme/Q1.py
@@ -1,8 +1,12 @@
 def func(*args):
     x = []
-    x += args
+    x += args #solução cabulosa!!!
     qnt = len(x)
     print("A soma: {}\nA quantidade de argumentos informados: {}".format(sum(x), qnt))
+'''
+    qnt = len(args)
+    print("A soma: {}\nA quantidade de argumentos informados: {}".format(sum(args), qnt))
+'''
 
 
-func(1, 2, 3)
+func(1, 2, 3, 3.5)
