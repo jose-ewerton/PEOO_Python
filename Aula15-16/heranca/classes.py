22cf68e7ea1473e3a5431b7f1eba02a1f29d3ee1
jose-ewerton <jose-ewerton@escolar.ifrn.edu.br>
2021-03-30 16:20:36 -0300

    Update comentários e explicações

--- Aula15-16/heranca/classes.py
+++ Aula15-16/heranca/classes.py
@@ -1,10 +1,17 @@
 class Mamifero (object): #sempre é bom que as suas classes herdem de objetc do python
-
+    num = 10
     def __init__(self, nome="Animal",peso = 0):
         self.nome = nome
         self.peso = peso
     def alimentacao_filhote(self):
         print(f"O {self.nome} quando FILHOTE, alimenta-se com Leite")
+        self.peso += Mamifero.num  #exemplo de atributos de classe e acesso a esses atributos
+        print(f"novo pesso {self.peso}")
+        #self.peso += num
+
+    #método abstrato
+    def comportamento(self):
+        pass
 
 
 class Gato(Mamifero): #Gato herda os atributos e métodos da classe manifero (extends), mas não tem construtor
@@ -20,3 +27,18 @@ class Cachorro(Mamifero):
     
     def alimentacao_adulta(self):
         print(f"O {self.nome} quando ADULTO, alimenta-se com CARNE")
+
+    @classmethod
+    def metodo_classe(cls):  #manipular funções globais da classe
+        print("Acesso metodo de classe!")
+
+    @staticmethod
+    def metodo_estatico(): #não faz o self e é statico
+        print("Acesso metodo estatico!")
+
+'''
+    def comportamento(self):
+        super().comportamento()
+        print("Esse animal é doméstico!")
+
+        '''
--- Aula15-16/heranca/main.py
+++ Aula15-16/heranca/main.py
@@ -17,20 +17,96 @@ c = Cachorro("Cachorro",20)
 print(c.nome,c.peso,c.som)
 c.alimentacao_filhote()
 c.alimentacao_adulta()
+c.comportamento()
+c.metodo_classe()
+c.metodo_estatico()
 
 '''
-#herança múltipla e polimorfismo
+
 Lista 1,2,3,4,5,6
 50% da nota
 
 Mini-projeto individual
 50%
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 Aquecimento - Defina uma classe ObjetoGrafico com atributos cor_de_preenchimento, preenchida, cor de contorno
 
 3 classe Retangulo, Circulo e Triangulo
 Todas devem ser subclasses de ObjetoGrafico e possuir os métodos área e perímetro
 
+
+Metodos abstrato - criar uma classe e manda a tarefa para outra classe definir (Subclasses)
+
+
+Metodos estáticos - não tem o self e só pode ser chamado pela classe
+
+Conta.imprimir()
+Nos metodos de classe também não são acessiveis, apenas com 
+class Conta:
+    num = 1000
+
+    def teste(self):
+        return Conta.num
+
+
+Atributos da Classe - 
+
+class Cachorro:
+    nome = 'Rex'
+    cor = 'marrom'
+dog = Cachorro()
+dog.nome
+dog.cor
+
+Cachorro.nome
+Cachorro.cor
+
+
 '''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
