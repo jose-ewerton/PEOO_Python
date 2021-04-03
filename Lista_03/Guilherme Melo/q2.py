460775f3ca02462286131f24716ff697993e299a
Gui69 <80356025+Gui69@users.noreply.github.com>
2021-04-03 14:46:06 -0300

    Add files via upload

---
+++ Lista_03/Guilherme Melo/q2.py
@@ -0,0 +1,8 @@
+#Faça uma função que informe a quantidade de dígitos de um determinado número inteiro informado.
+
+def quantidade (a):
+    v = len (a)
+    print("A quantidade de dígitos do número fornecido é", v)
+
+
+quantidade(str(input ("Digite um numero inteiro: ")))
