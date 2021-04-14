ec9a22ddcbee521654ef8152d04a8d367d9f16ab
jose-ewerton <jose-ewerton@escolar.ifrn.edu.br>
2021-04-14 15:30:08 -0300

    Update proejto M4

--- exemplo_proj_M4/form_reservas_society.py
+++ exemplo_proj_M4/form_reservas_society.py
@@ -7,8 +7,28 @@ from kivy.properties import ObjectProperty
 
 class Painel(FloatLayout):  
     layout = ObjectProperty(None) 
+    label1 = ObjectProperty(None)
+    label2 = ObjectProperty(None)
+    label3 = ObjectProperty(None)
+    label4 = ObjectProperty(None)
+    textinput1 = ObjectProperty(None)
+    textinput2 = ObjectProperty(None)
+    textinput3 = ObjectProperty(None)
+    textinput4 = ObjectProperty(None)
     def adicionar(self):
-        pass
+        self.label1.opacity = 1
+        self.label2.opacity = 1
+        self.label3.opacity = 1
+        self.label4.opacity = 1
+        self.textinput1.opacity = 1
+        self.textinput2.opacity = 1
+        self.textinput3.opacity = 1
+        self.textinput4.opacity = 1
+
+        if (self.textinput1.text == ""):
+            print("Teste do vazio")
+        else:
+            print(f'Text é: {self.textinput1.text}')
 
 class painelApp(App):
     def build(self):
