e62f900ee3e5792f5dcf4810260dd9a5b3cb198b
kirito2002 <80356903+limapedro2002@users.noreply.github.com>
2021-04-15 12:40:43 -0300

    at5q1

---
+++ Lista_05/Pedro Lucas/quest1_veículos.py
@@ -0,0 +1,18 @@
+class carro:
+    def __init__(self, Marca, Modelo, Ano):
+        self.marca = Marca
+        self.modelo = Modelo
+        self.ano = Ano
+
+    def base(self):
+        a = carro
+
+        a.marca = input ("Digite a marca do carro:")
+        a.modelo = input ("Digite o modelo do carro:")
+        a.ano = input ("Digite o ano do carro:")
+
+        print ('\033[7;97m' + "Marca:", a.marca)
+        print ("Modelo:", a.modelo)
+        print ("Ano:", a.ano + '\033[0;0m')
+
+carro.base(1)
