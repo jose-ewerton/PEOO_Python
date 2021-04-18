29eca3f813ca312983a90aac088f27d14ec216ad
luismiguel69420 <80356420+luismiguel69420@users.noreply.github.com>
2021-04-18 11:21:14 -0300

    Update Main.py

--- Lista_06/Luis M_Guilherme Melo/Questao_3/Main.py
+++ Lista_06/Luis M_Guilherme Melo/Questao_3/Main.py
@@ -1,1 +1,13 @@
+from Endereco import Pessoa, Endereco
 
+
+gente = Pessoa(69420, "luis", 704)
+gente2 = Pessoa(1113, "pinho", 190)
+endereco1= Endereco(294, "cap jose", "cm", "RN", "Brazil")
+endereco2= Endereco(294, "cap jose", "cm", "RN", "Brazil")
+endereco3= Endereco(420, "cracolandia", "Sao Paulo", "SP", "Brazil")
+gente.inserir_endereco(endereco1)
+gente.inserir_endereco(endereco2)
+gente2.inserir_endereco(endereco3)
+gente.listar_enderecos()
+gente2.listar_enderecos()
