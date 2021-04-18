3e9e313ee5e257c5d0cd65de49a753c563d457b3
luismiguel69420 <80356420+luismiguel69420@users.noreply.github.com>
2021-04-18 10:56:53 -0300

    Update Main.py

--- Lista_06/Luis M_Guilherme Melo/Questao_2/Main.py
+++ Lista_06/Luis M_Guilherme Melo/Questao_2/Main.py
@@ -1,1 +1,8 @@
 
+from Revista import Revista
+
+
+revista1 = Revista(15, "Jueline na cozinha", "adulta", "playboy")
+revista1.inserir_edicao1('55', '20/12/2020', '5')
+revista1.detalhar_revista()
+revista1.mostrar_edicao1()
