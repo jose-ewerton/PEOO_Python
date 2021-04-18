db1757420dded8b0b14896e5a29d4245fc5a298b
Maria Clara <melo.clara@escolar.ifrn.edu.br>
2021-04-17 22:01:29 -0300

    Create exe4.py

---
+++ Lista_04/Maria Clara/exe4.py
@@ -0,0 +1,11 @@
+import módulo
+
+módulo.soma(1, 2, 3)
+módulo.acrescimo(525)
+módulo.concat("eli", "za", "be", "th")
+módulo.func(nome='Mary', sobrenome='styles')
+
+help(módulo.soma)
+help(módulo.acrescimo)
+help(módulo.concat)
+help(módulo.func)
