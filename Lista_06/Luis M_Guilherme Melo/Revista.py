c48570ca94367d3d0ae5060925b1f7206be09de9
luismiguel69420 <80356420+luismiguel69420@users.noreply.github.com>
2021-04-18 10:38:24 -0300

    Update and rename revista teste.py to Revista.py

--- Lista_06/Luis M_Guilherme Melo/Revista.py
+++ Lista_06/Luis M_Guilherme Melo/Revista.py
@@ -61,7 +61,3 @@ class Edicao:
         
 
 
-revista1 = Revista(15, "Jaqueline na cozinha", "adulta", "playboy")
-revista1.inserir_edicao1('55', '20/12/2020', '5')
-revista1.detalhar_revista()
-revista1.mostrar_edicao1()
