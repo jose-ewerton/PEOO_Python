c48e45fdc42530255ea441bb7162848067dad5d8
Maria Clara <melo.clara@escolar.ifrn.edu.br>
2021-04-20 16:22:42 -0300

    Update exe1.py

--- Lista_01/Maria Clara/exe1.py
+++ Lista_01/Maria Clara/exe1.py
@@ -1,46 +1,36 @@
-soma = 0
-media = 0
-homens = 0
-menos = 0
-baixo = ''
-mulher = 0
-alta = ''
-homem = 0
-pesado = ''
-percem = 0
-percef = 0
-totH = 0
-totM = 0
-cont = 0
-for c in range(1, 3):
+altura=[]
+peso=[]
+sexo=[]
+alf=[]
+alm=[]
+pm=[]
+for c in range(1, 16):
     print(f'-=-=-=-=-=- {c}ª Pessoa -=-=-=-=-=-= ')
-    nome = str(input('Qual é o seu nome: '))
-    altura = float(input('Qual é a sua altura: '))
-    peso = float(input('Qual é seu peso: '))
-    sexo = str(input('Qual o seu gênero (M/F): '))
-    soma += altura
-    cont += 1
-    if altura > 170 and sexo == 'Mm':
-        homens += 1
-    if c > altura and sexo in 'Mm':
-        menos = altura
-        baixo = nome
-    if c < altura and sexo in 'Ff':
-        mulher = altura
-        alta = nome
-    if c > peso and sexo in 'Mm':
-        homem = peso
-        pesado = nome
-    elif sexo in 'Ff':
-        totM += 1
-        percef = 100 * (c/totM)
-    elif sexo in 'Mm':
-        totH += 1
-        percem = 100 * (c/totH)
-media = soma / 2
-print(f'A média de altura do grupo {media:.2f}')
-print(f'O nome da mulher mais alta: {alta}')
-print(f'O nome do homem mais baixo: {baixo}')
-print(f'O número de homens com mais de 1,70 m de altura: {homens}')
-print(f'O homem mais pesado: {pesado}')
-print(f'O percentual de homens: {percem} e o de mulheres: {percef} ')
+    altura.append(float(input("digite o valor de sua altura: ")))
+    peso.append(float(input("digite o valor do seu peso: ")))
+    a=input("digite seu sexo(M ou F): ").upper()
+    while a!="M" and a!="F":
+        a=input("digite seu sexo(M ou F): ").upper()
+    sexo.append(a)
+print("Media de altura do grupo: ",sum(altura)/len(altura))
+nm=0
+nh=0
+for i in range(len(sexo)):
+    if sexo[i]=="F":
+        alf.append(altura[i])
+        nm=nm+1
+    elif sexo[i]=="M":
+        alm.append(altura[i])
+        pm.append(peso[i])
+        nh=nh+1       
+print("Mulher mais alta: ",max(alf))
+print("Homem mais baixo: ",min(alm))
+con = 0
+for b in range(len(alm)):
+    if alm[b]>1.70:
+        con = con+1    
+print("quantidade de homens com mais de 1.70m: ",con)
+print("Homem mais pesado: ",max(pm))
+print("percentual de mulheres: ",(100*nm)/15,"%")
+print("percentual de homens:",(100*nh)/15,"%")
+   
