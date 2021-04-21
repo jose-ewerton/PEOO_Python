2fc84c55ce254a3cb9312738ea2565a174b59125
Pedro Gabriel <gabriel.medeiros1@escolar.ifrn.edu.br>
2021-04-21 14:40:46 -0300

    questao_1(a,b,c)

---
+++ Lista_05/Pedro Gabriel/questao_01_veiculo.py
@@ -0,0 +1,23 @@
+"""Defina os atributos e métodos da seguinte classe:
+Veiculos:
+"""
+
+
+class carro:
+    def __init__(self, Marca, Modelo, Ano):
+        self.marca = Marca
+        self.modelo = Modelo
+        self.ano = Ano
+
+    def base(self):
+        a = carro
+
+        a.marca = input ("Digite a marca do carro: ")
+        a.modelo = input ("Digite o modelo do carro: ")
+        a.ano = input ("Digite o ano do carro: ")
+
+        print ('\033[7;97m' + "Marca:", a.marca)
+        print ("Modelo:", a.modelo)
+        print ("Ano:", a.ano + '\033[0;0m')
+
+carro.base(1)
