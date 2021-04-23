d7d1386d47bc6e19bfbd6cd96736dc8a4a33a462
Pedro Gabriel <gabriel.medeiros1@escolar.ifrn.edu.br>
2021-04-23 14:49:24 -0300

    lista_6_questoes(1,2)

---
+++ Lista_06/Pedro Gabriel _ Viniciuis Barbosa/questao_02/executar.py
@@ -0,0 +1,7 @@
+from edicao_e_revista import Revista
+from edicao_e_revista import Edicao
+
+revista1 = Revista('6873682378','Como pegar mestre?','Free Fire','#1',)
+edicao1 = Edicao('7','13/07/19','15')
+revista1.adc_revista(edicao1)
+revista1.listar_edicoes()
