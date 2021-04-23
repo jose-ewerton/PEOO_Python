16be670eb1ffb7669280fdcf1686ea5220368100
kirito2002 <80356903+limapedro2002@users.noreply.github.com>
2021-04-23 11:28:13 -0300

    delet

--- Lista_06/Felipe Victor e Pedro Lucas/Q3/endereço.py
+++
@@ -1,7 +0,0 @@
-class Endereco:
-    def __init__(self, num_casa, rua, cidade, estado, pais):
-        self._num_casa = num_casa
-        self._rua = rua
-        self._cidade = cidade
-        self._estado = estado
-        self._pais = pais
