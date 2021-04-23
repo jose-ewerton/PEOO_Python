15126d9002116740571bea638350343019d753e5
kleytianezz <kleytiane.o@escolar.ifrn.edu.br>
2021-04-22 21:13:32 -0300

    Create list3.1.py

---
+++ Lista_03/Kleytiane Silva/list3.1.py
@@ -0,0 +1,13 @@
+def soma3(par1, par2, par3, par4):
+    return (par1 + par2 + par3 + par4)
+
+numero1 = int(input("Digite um número:"))
+numero2 = int(input("Digite um número:"))
+numero3 = int(input("Digite um número:"))
+numero4 = int(input("Digite um número:"))
+
+print("a soma é", soma3(numero1,numero2, numero3, numero4))
+def soma3(a, b, c, d):
+    s = a + b + c + d
+    return s
+
