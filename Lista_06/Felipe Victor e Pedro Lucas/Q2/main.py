c5d40f54857d2669f43593ccc0ebfd89e17c6b91
kirito2002 <80356903+limapedro2002@users.noreply.github.com>
2021-04-23 11:27:03 -0300

    Delete Lista_06/Felipe Victor e Pedro Lucas directory

--- Lista_06/Felipe Victor e Pedro Lucas/Q2/main.py
+++
@@ -1,7 +0,0 @@
-from revista import Revista
-from edição import Edicao
-
-revista1 = Revista('01','Adiquira o Curso','espanhol','#2000',)
-edicao1 = Edicao('69','20/02/29','24')
-revista1.adc_revista(edicao1)
-revista1.listar()
