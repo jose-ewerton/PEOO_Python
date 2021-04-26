392a919f66577fb2cbe100bc7d7ae7086c4f213d
Maria Clara <75913777+Clara-ang@users.noreply.github.com>
2021-04-26 16:52:40 -0300

    Update pessoa.py

--- Lista_06/Kleytiane Silva e Maria Clara/Q3/pessoa.py
+++ Lista_06/Kleytiane Silva e Maria Clara/Q3/pessoa.py
@@ -9,35 +9,17 @@ class Pessoa:
         self.__lista_endereco.append (endereco)
 
     def listar(self):
-        print("Nome:", self.__nome, "CPF:", self.__cpf, "RG:", self.__identidade)
+        print("Nome:", self.__nome)
+             ("CPF:", self.__cpf) 
+             ("RG:", self.__identidade)
         print("~~~~~~~~~~Endereços~~~~~~~~~~")
-        for endereco in self.__lista_endereco:
-            
+        for endereco in self.__lista_endereco:          
             print("número de residencia:", endereco._num_casa)
             print("Rua:", endereco.__rua)
             print("Cidade:", endereco._cidade)
             print("Estado:", endereco._estado)
             print("País:", endereco._pais)
-class pessoa:
-    def __init__(self ,nome, cpf, identidade):
-        self.__nome = nome
-        self.__cpf = cpf
-        self.__identidade = identidade
-        self.__lista_endereco =  []    
 
-    def adc(self, endereco):
-        self.__lista_endereco.append(endereco)
 
-    def listar(self):
-        print(" Pessoa:") 
-        print("Nome:", self.__nome.format(self.__nome,))
-        print("CPF:",self.__cpf.format(self.__cpf,))
-        print("RG:", self.__identidade .format(self.__identidade))    
-        print("Endereços:")
-        for endereco in self.__lista_endereco:
-            print("Rua:", endereco ._rua)
-            print("Número de residencia:", endereco._num_casa)
-            print("Cidade:", endereco._cidade)
-            print("Estado:", endereco._estado)
-            print("País:", endereco._pais) 
+   
             
