fa4275412732216ccb10dc6275905d27991ea728
gqbrriela <gabriela.campos@escolar.ifrn.edu.br>
2021-04-26 18:29:23 -0300

    questao 2

---
+++ Lista_06/Gabriela Campos e Yasmin Lima/questao 2/main.py
@@ -0,0 +1,7 @@
+from revista import Edicao, Revista
+
+revista=Revista(1, "Diário da fama", 10)
+revista.adicionar_edicao(17, "13/10/10",3)
+revista.adicionar_edicao(6,"Festa",7)
+revista.adicionar_edicao(2,"Viagem",5)
+revista.apresentar()
