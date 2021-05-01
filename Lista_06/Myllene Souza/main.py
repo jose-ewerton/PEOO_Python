6dee405f4576181edbaece847395373d360f3e97
myllene02 <80775436+Myllene02@users.noreply.github.com>
2021-05-01 01:51:40 -0300

    Add files via upload
    Manu_Myllene

--- Lista_06/Myllene Souza/main.py
+++ Lista_06/Myllene Souza/main.py
@@ -1,44 +1,12 @@
-from produto import Produto
-from item_pedido import ItemPedido
-from pedido import Pedido
+from Pessoa_3 import Pessoa
+from Endereco import Endereco
 
-p1 = Produto("00001", "Maça", 5.00)
-p2 = Produto("00002", "batata", 7.00)
-p3 = Produto("00003", "Prestigio",5.40)
+e1 = Endereco ( "N" "22", "Maria Augusta", "Ceará-mirim","Rn", "Brasil")
+e2 = Endereco ( "23", "Capitão da Penha", "Natal","Rn", "Brasil")
+e3 = Endereco ( "24", "São José", "Massaranbuba","Rn", "Brasil")
+e4 = Endereco ( "25", "Rafael Rodriguis", "Ceará-mirim","Rn", "Brasil")
+#e.endereco_detalar ()
 
-condicao = True
-
-pedido = Pedido()
-
-while condicao == True:
-    print("Bom dia usuário, segue lista de produtos")
-    print("Produto 01: ",p1.codigo,p1.descricao,p1.valor)
-    print("Produto 02: ",p2.codigo,p2.descricao,p2.valor)
-    print("Produto 03: ",p3.codigo,p3.descricao,p3.valor)
-    escolha = input("Escolha 1 para adicionar e 2 para remover da lista: ")
-    if (escolha == '1'):
-        codigo = input("Digite o código do produto que deseja: ")
-        quant = int(input("Digite a quantidade desse produto: "))
-        if (codigo == p1.codigo):
-            pedido.adicionar_item(p1,quant)
-        elif (codigo == p2.codigo):
-            pedido.adicionar_item(p2,quant)
-        elif (codigo == p3.codigo):
-            pedido.adicionar_item(p3,quant)
-        else:
-            print("Insira um código válido!")
-    elif (escolha == '2'):
-        if (len(pedido.lista_pedidos) == 0):
-            print("Lista de pedidos vazia!")
-        else:
-            print("Entra na função")
-            pedido.remover_item()       
-    condicao = True
-
-'''
-p1.detalha_produto()
-p2.detalha_produto()
-p3.detalha_produto()
-'''
-
+p= Pessoa ("Juremisvaldo", "000.000.000-00", e1,e2,e3,e4) 
+p.detalhar_pessoa ()
 
