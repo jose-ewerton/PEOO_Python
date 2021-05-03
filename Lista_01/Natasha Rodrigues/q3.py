0ce89f0199ad1e98739d7fed259ea8db432de226
Natasha-Silva <natasha.silva@escolar.ifrn.edu.br>
2021-05-03 13:50:08 -0300

    Create q3.py

---
+++ Lista_01/Natasha Rodrigues/q3.py
@@ -0,0 +1,32 @@
+'''
+Faça um programa que receba a idade e a opinião de 15 espectadores de um cinema em relação
+a um determinado filme, sendo ótimo - 3, bom - 2, regular -1, em seguida calcule e informe:
+a. A média das idades das pessoas que responderam ótimo;
+b. A quantidade de pessoas que responderam regular;
+c. A porcentagem de pessoas que responderam bom entre todos os espectadores
+analisados.
+'''
+somidade = 0
+medidade = 0
+totregular = 0
+porbom = 0
+bom = 0 
+for p in range(1, 16):
+    print('----- {}° Pessoa -----'.format(p))
+    idade = int(input('Informe sua idade:'))
+    opiniao = str(input('Informe sua opinião em relação ao filme. Digite 3 para ótimo, 2 para bom e 1 para regular:')) 
+
+    if opiniao in '3':
+        somidade += idade
+        medidade = somidade / 15
+    if opiniao in '1':
+        totregular += 1
+    if opiniao in '2':
+        bom += 1
+    
+porbom = bom*100 / 15
+    
+print('-'*12)
+print('A média de idade dos espectadores que responderam "ótimo" é de {}'.format(medidade))
+print('A quantidade de pessoas que respoderam "regular" foi {}'.format(totregular))
+print('A porcentagem de pessoas que responderam "bom" entre todos os espectadores analisados foi de {}%'.format(porbom))
